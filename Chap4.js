b6289ab2161025d2047c238c6f8b2cdf7f2882c3
Parthib Chandra <106731257+parthibChandra@users.noreply.github.com>
2022-06-03 20:54:00 +0530

    Create Chap4.js

---
+++ Chap4.js
@@ -0,0 +1,26 @@
+function range(s, e, step){
+    let arr=[];
+    
+    if (step === undefined) step=1;
+    if (e<s && step>0) step=-step;
+    if (e>s && step<0) step=-step;
+    for (let i = s; Math.abs(s-i)+Math.abs(e-i)===Math.abs(s-e); i+=step) {
+        arr.push(i);
+    }
+    console.log(arr);
+    return arr;
+}
+
+function sum(set) {
+    let t=0;
+    for (let nums of set) {
+        t+=nums;
+    }
+   	return t;
+}
+console.log(sum(range(1,10)))
+console.log(sum(range(10,1)))
+console.log(sum(range(1,10,3)))
+console.log(sum(range(1,10,-3)))
+console.log(sum(range(10,1,3)))
+console.log(sum(range(10,1,-3)))
